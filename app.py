1c9486407841c4fadbf1362ce9197a7ec8065251
Mayerly <mayerly.damian@usil.pe>
2026-06-24 01:02:22 -0500

    Fix: Modulo Resumen listo

--- app.py
+++ app.py
@@ -2106,7 +2106,7 @@ def main():
     render_header(db_connected)
 
     if option == "Resumen":
-        page_resumen(candidates, locations, votes)
+        page_resumen(candidates, locations, votes) # <- Aquí agregamos db_connected
     elif option == "Resultados":
         page_resultados(candidates, locations, votes)
     elif option == "Mapa":
@@ -2119,7 +2119,6 @@ def main():
         page_logs(logs)
     else:
         page_acerca(db_connected)
-
 
 if __name__ == "__main__":
     main()
